c7e10060eb35a6f9b29e1e97f06d231e0af50347
H-Alexa <tatineesarkersunom@gmail.com>
2023-09-22 02:25:45 +0600

    _

---
+++ ClassWork/Class 1/c2.js
@@ -0,0 +1,2 @@
+var name = "Tatinee";
+console.log(name);
---
+++ ClassWork/Class 1/test.js
@@ -0,0 +1,20 @@
+console.log("Meow");
+
+//1
+// Intro to Arrow function
+
+// Normal function
+
+// function square(number){
+//     return number*number;
+// }
+
+// Arrow function
+
+// const square = (number) =>  number*number; 
+
+// Driver code
+// console.log(square(8));
+
+// 2
+// Object
