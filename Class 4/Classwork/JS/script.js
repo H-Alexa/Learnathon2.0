6d32a9e834c2b80407be05a57edd1f3ade3211d1
H-Alexa <tatineesarkersunom@gmail.com>
2023-09-26 01:10:46 +0600

    changed layout

---
+++ Class 4/Classwork/JS/script.js
@@ -0,0 +1,8 @@
+console.log('Meow');
+console.log('Meow1');
+console.log('Meow2');
+setTimeout(()=>{
+    console.log('Meow3');
+},0);
+console.log('Meow4');
+
---
+++ Class 4/Classwork/JS/test.js
@@ -0,0 +1,26 @@
+// var a = 10;
+// {
+//     let a = 100;
+//     console.log(a);
+// }
+// console.log(a);
+// let a = 10;
+// var ac= 1;
+// // {
+// //     let a = 100;
+// //     console.log(a);
+// // }
+// {
+//     let b=20;
+//     {
+//         let c=30;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// console.log(a);
+
+let b = 10;
+{
+    var a = 20;
+}
