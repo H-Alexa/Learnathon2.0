1c8d396eaf3c41d4a755af771031a7bde96c7d69
H-Alexa <tatineesarkersunom@gmail.com>
2023-11-05 05:30:52 +0600

    Automatic Global variable example added with explanations

--- JS/Class 2/Homework/js/variables/automaticGlobal.js
+++ JS/Class 2/Homework/js/variables/automaticGlobal.js
@@ -1,8 +1,16 @@
-// console.log(carName); //here carName isn't hoisted yet as the function isn't invoked
+// console.log(carName); //here carName isn't hoisted yet though being a global variable as the function isn't invoked yet
 myFun();
 function myFun()
 {
-    carName = "Toyota";
+    carName = "Toyota"; //carName isn't declared, that's why it goes to global scope and is accessible from anywhere like any other global variables. No function scope is applied in this
     console.log(carName);
 }
 console.log(carName);
+
+function anotherFunc()
+{
+    var carAnother = "Volvo";
+    console.log(carAnother);
+}
+anotherFunc();
+console.log(carAnother); //This will show uncaught refError because here var was in function scope and it's not accessible from outside
