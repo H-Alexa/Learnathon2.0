d73a887a5d469636748365ce76aec1c8531d752e
H-Alexa <tatineesarkersunom@gmail.com>
2023-11-05 06:28:34 +0600

    finished lexical environment

--- JS/Class 2/Homework/js/scope/lexEnv/e1.js
+++ JS/Class 2/Homework/js/scope/lexEnv/e1.js
@@ -1,10 +1,14 @@
 function a() {
     console.log(b); //At first js will try to look for b in the local scope
-    //It's not here.  
+    //It's not here. Then it will look up in global scope and will take b's value and log that here
+    var d = 100;
     function c() {
         console.log(b);
+        console.log(d); //it tries to find d in local environment.Then if not found goes to the lexical environment of it's parent, and upon finding prints the value
     }
+    c();
 }
 
 var b = 10;
 a();
+// console.log(d); //D uncaught error as it's out of it's function scope
